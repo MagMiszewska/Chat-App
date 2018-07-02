f7656909977c14a7943578079646db28ed673ee4
Magdalena Miszewska <mag.miszewska@gmail.com>
2018-07-02 10:15:22 +0200

    Update App.js file

--- client/App.js
+++ client/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import io from 'socket.io-client';
+import styles from './App.css';
+
+import MessageForm from './MessageForm';
+import MessageList from './MessageList';
+import UsersList from './UsersList';
+import UserForm from './UserForm';
 
 const socket = io('/');
 
@@ -7,6 +13,31 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = { users: [], messages: [], text: '', name: '' };
+    }
+
+    componentDidMount() {
+        socket.on('message', message => this.messageReceive(message));
+        socket.on('update', ({ users }) => this.chatUpdate(users));
+    }
+
+    messageReceive(message) {
+        const messages = [message, ...this.state.messages];
+        this.setState({ messages });
+    }
+
+    chatUpdate(users) {
+        this.setState({ users });
+    }
+
+    handleMessageSubmit(message) {
+        const messages = [message, ...this.state.messages];
+        this.setState({ messages });
+        socket.emit('message', message);
+    }
+
+    handleUserSubmit(name) {
+        this.setState({ name });
+        socket.emit('join', name);
     }
 
     render() {
@@ -40,6 +71,10 @@ class App extends Component {
                 </div>
             </div>
         );
+    }
+
+    renderUserForm() {
+        return (<UserForm onUserSubmit={name => this.handleUserSubmit(name)} />)
     }
 };
 
